6483f8fcfeb2a5aceeaf87117cb1428550ea669c
Sosthène NOUEBISSI <sosthenenouebissi@gmail.com>
2025-10-30 00:30:54 +0100

    Prepare login/register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Login } from './features/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Login',
+        component: Login
+    }
+];
